8ca38738af973919de040a24564132af22ecb446
Natohiri88 <salahudhinatohiri88@gmail.com>
2025-02-05 22:41:15 +0700

    submit_registration.php

--- submit_registration.php
+++ submit_registration.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = htmlspecialchars($_POST['subject']);
 
     // Tentukan alamat email penerima
-    $to = "your-email@example.com";  // Ganti dengan alamat email tujuan
+    $to = "syahnianurjamalia@gmail.com";
 
     // Tentukan subjek email
     $subject_email = "Pendaftaran Jasa Les Online - $name";
